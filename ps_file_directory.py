edac70605b24f3681a614771bc625eac9a84c359
Peter Senyk <Peter.Senyk@gmail.com>
2024-06-03 14:33:22 -0700

    EDIT for multiline directories

--- ps_file_directory.py
+++ ps_file_directory.py
@@ -1,9 +1,3 @@
-"""
-Program to find files in a file directory tree and return file paths
-@author: Peter Senyk
-@version: 2024_0.2
-"""
-
 import csv
 from datetime import datetime
 
@@ -11,22 +5,42 @@ from datetime import datetime
 def find_file_in_tree(file_path, filename):
     with open(file_path, 'r', encoding='utf-16') as file:
         lines = file.readlines()
-    file_path_dictionary = check_lines(lines[::-1], filename)
+    file_path_dictionary = check_lines(lines, filename)
     return file_path_dictionary
 
 
 def check_lines(lines, filename):
     file_path_dictionary = {}
+    current_dir = ''
     count = 0
-    for line in lines:
-        if "Directory:" in line and count > 0:
-            dir_line = clean_directory_line(line)
-            file_path_dictionary[dir_line] = count
+
+    for i, line in enumerate(lines):
+        if "Directory:" in line:
+            if current_dir and count > 0:
+                file_path_dictionary[current_dir] = count
+            current_dir = clean_directory_line(line.strip())
+            current_dir = check_multiline_directory(lines, i + 1, current_dir)
             count = 0
+        elif line.strip() == '' and count > 0:
+            if current_dir:
+                file_path_dictionary[current_dir] = count
+                current_dir = ''
+                count = 0
         elif filename.lower() in line.lower():
             count += 1
+
+    if current_dir and count > 0:
+        file_path_dictionary[current_dir] = count
+
     print(file_path_dictionary)
     return file_path_dictionary
+
+
+def check_multiline_directory(lines, index, current_dir):
+    while index < len(lines) and lines[index].strip() != '' and "Directory:" not in lines[index]:
+        current_dir += lines[index].strip()
+        index += 1
+    return current_dir
 
 
 def clean_directory_line(line):
@@ -55,7 +69,7 @@ def export_file_paths(path_dictionary, output_path):
 if __name__ == '__main__':
     tree_file_path = r"D:\testScripts\batch\directory_listing.txt"  # Path to the tree file
     output_file_path = r"D:\testScripts\testOutput"  # Output file path
-    search_filename = "sandbox"  # Filename to search for
+    search_filename = "syntychristmas"  # Filename to search for
 
     file_paths = find_file_in_tree(tree_file_path, search_filename)
     export_file_paths(file_paths, output_file_path)
