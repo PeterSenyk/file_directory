39fc112d016b8946d8f3f7cdb503b53198f6eab3
Peter Senyk <Peter.Senyk@gmail.com>
2024-06-03 10:36:25 -0700

    ADD export function

--- ps_file_directory.py
+++ ps_file_directory.py
@@ -4,6 +4,9 @@ Program to find files in a file directory tree and return file paths
 @version: 2024_0.2
 """
 
+import csv
+from datetime import datetime
+
 
 def find_file_in_tree(tree_file_path, search_filename):
     file_path_dictionary = {}
@@ -11,7 +14,8 @@ def find_file_in_tree(tree_file_path, search_filename):
         lines = file.readlines()
     file_path_dictionary = check_lines(lines[::-1], search_filename)
     return file_path_dictionary
-    
+
+
 def check_lines(lines, search_filename):
     file_path_dictionary = {}
     count = 0
@@ -23,20 +27,34 @@ def check_lines(lines, search_filename):
             file_path_dictionary[dir_line] = count
             count = 0
     return file_path_dictionary
-        
+
+
 def clean_directory_line(line):
     dir_line = line.lstrip(" ").strip("Directory: ")
     return dir_line
-    
+
+
 def clean_dictionary_results(file_path_dictionary):
     for key, value in file_path_dictionary.items():
         print(f"{key}\nContains {value} files containing the search\n")
 
 
+def export_file_paths(path_dictionary, output_path):
+    current_date = datetime.now().strftime("%y-%m-%d")
+    file_name = f"Filepath_Output_{current_date}.csv"
+
+    with open(file_name, "w", newline="") as output_file:
+        writer = csv.writer(output_file)
+        writer.writerow(["File Path", "Number of Files"])
+
+        for path, number in path_dictionary.items():
+            writer.writerow([path, number])
+
+
 if __name__ == '__main__':
     tree_file_path = r"C:\Users\SENYKP\Desktop\Projects\file_directory_script\F_PowerShell\directory_listing.txt"  # Path to the tree file
     output_file_path = r"C:\Users\SENYKP\Desktop\Projects\file_directory_script\Test_Output\TestOutput"  # Output file path
-    search_filename = 'Campbell'  # Filename to search for
+    search_filename = input("Enter your search: ")  # Filename to search for
 
-    file_path_set = find_file_in_tree(tree_file_path, search_filename)
-    export_file_paths(file_path_set, output_file_path)
+    file_paths = find_file_in_tree(tree_file_path, search_filename)
+    export_file_paths(file_paths, output_file_path)
