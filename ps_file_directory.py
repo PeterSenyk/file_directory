59d18c75d61156ad5177a00baf1eb23ab7fc10d8
Peter Senyk <Peter.Senyk@gmail.com>
2024-06-03 12:02:00 -0700

    EDIT clean_lines

--- ps_file_directory.py
+++ ps_file_directory.py
@@ -9,7 +9,7 @@ from datetime import datetime
 
 
 def find_file_in_tree(file_path, filename):
-    with open(file_path, 'r', encoding='utf-8') as file:
+    with open(file_path, 'r', encoding='utf-16') as file:
         lines = file.readlines()
     file_path_dictionary = check_lines(lines[::-1], filename)
     return file_path_dictionary
@@ -19,17 +19,18 @@ def check_lines(lines, filename):
     file_path_dictionary = {}
     count = 0
     for line in lines:
-        if filename in line:
+        if filename.lower() in line.lower():
             count += 1
-        elif "Directory" in line and count > 0:
+        elif "Directory:" in line and count > 0:
             dir_line = clean_directory_line(line)
             file_path_dictionary[dir_line] = count
             count = 0
+    print(file_path_dictionary)
     return file_path_dictionary
 
 
 def clean_directory_line(line):
-    dir_line = line.lstrip(" ").strip("Directory: ")
+    dir_line = line.removeprefix("    Directory: ")
     return dir_line
 
 
@@ -52,9 +53,9 @@ def export_file_paths(path_dictionary, output_path):
 
 
 if __name__ == '__main__':
-    tree_file_path = r"D:\testScripts\powershell_tree.txt"  # Path to the tree file
+    tree_file_path = r"D:\testScripts\batch\directory_listing.txt"  # Path to the tree file
     output_file_path = r"D:\testScripts\testOutput"  # Output file path
-    search_filename = input("Enter your search: ")  # Filename to search for
+    search_filename = "sandbox"  # Filename to search for
 
     file_paths = find_file_in_tree(tree_file_path, search_filename)
     export_file_paths(file_paths, output_file_path)
