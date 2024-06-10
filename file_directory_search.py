d952d36e9a232d6b7c46cdbf1182dafdfd41c40d
Peter Senyk <Peter.Senyk@gmail.com>
2024-06-10 12:08:43 -0700

    ADD docstings

--- file_directory_search.py
+++ file_directory_search.py
@@ -1,5 +1,5 @@
 """
-Program to search a file directory and return file paths
+Program to search a PowerShell recursive file directory and return file paths
 @author: Peter Senyk
 @version: 2024_0.3
 """
@@ -8,6 +8,13 @@ import csv
 
 
 def find_file_in_tree(file_path, filename):
+    """
+    Reads the file directory textfile and searches for given filename.
+    :param file_path: Path to the file directory text file.
+    :param filename:  Filename to search for.
+    :return: A dictionary where the key represents a filepath, and the value represents the number of matches at
+    that location.
+    """
     try:
         with open(file_path, 'r', encoding='utf-16') as file:
             lines = file.readlines()
@@ -22,9 +29,17 @@ def find_file_in_tree(file_path, filename):
 
 
 def process_lines(lines, filename):
+    """
+    Processes the lines from the directory text file and searches for the filename.
+    :param lines: List of lines from the directory file.
+    :param filename: String representing the filename being searched for.
+    :return: A dictionary where the key represents a filepath, and the value represents the number of matches at
+    that location.
+    """
     file_path_dictionary = {}
     current_dir = ''
     count = 0
+    print("Searching...", end="")
 
     for i, line in enumerate(lines):
         if "Directory:" in line:
@@ -42,10 +57,18 @@ def process_lines(lines, filename):
         elif filename.lower() in line.lower():
             count += 1
 
+    print("Complete")
     return file_path_dictionary
 
 
 def check_multiline_directory(lines, index, current_dir):
+    """
+    Checks for paths that may consist of multiple lines in the directory file.
+    :param lines: List of lines from the directory file.
+    :param index: An int representing the initial line the "Directory" keyword was found on.
+    :param current_dir: A string representing the current file path being searched.
+    :return: A string representing the current directory path being searched.
+    """
     while index < len(lines) and lines[index].strip() != '' and "Directory:" not in lines[index]:
         current_dir += lines[index].strip()
         index += 1
@@ -53,15 +76,29 @@ def check_multiline_directory(lines, index, current_dir):
 
 
 def clean_directory_line(line):
+    """
+    Removes unnecessary parts of the directory path.
+    :param line: A string representing the current line being read.
+    :return: A string representing the total directory path.
+    """
     return line.removeprefix(" ").removeprefix("Directory:").removeprefix(" ")
 
 
-def clean_dictionary_results(file_path_dictionary):
-    for key, value in file_path_dictionary.items():
-        print(f"{key}\nContains {value} files containing the search\n")
+def total_results(path_dictionary):
+    path_count = len(path_dictionary)
+    file_count = 0
+    for files in path_dictionary.values():
+        file_count += files
+    print(f"{path_count} file paths were found\nContaining a total of {file_count} files/folders")
 
 
 def export_file_paths(path_dictionary, search, output_path):
+    """
+    Exports the filepaths to a CSV file.
+    :param path_dictionary: A dictionary holding the filepaths and the amount of files found at each path.
+    :param search: A string representing the filename searched for
+    :param output_path: A string representing the output path for the CSV file.
+    """
     file_name = f"Filepath_Output_For_Search_{search}.csv"
     file_output = f"{output_path}\\{file_name}"
     try:
@@ -71,6 +108,7 @@ def export_file_paths(path_dictionary, search, output_path):
 
             for path, number in path_dictionary.items():
                 writer.writerow([path, number])
+        print(f"CSV exported to {file_output}")
     except Exception as e:
         print(f"Error writing to file: {e}")
 
@@ -81,5 +119,6 @@ if __name__ == '__main__':
     search_filename = input("Enter your search here: ")  # Filename to search for
 
     file_paths = find_file_in_tree(tree_file_path, search_filename)
+    total_results(file_paths)
     export_file_paths(file_paths, search_filename, output_file_path)
     stay_open = input("Press Enter to exit...")
