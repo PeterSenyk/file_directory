dc8f1f0914f0b3d3f1bca8880299d40af2715e9e
Peter Senyk <Peter.Senyk@gmail.com>
2024-06-10 11:40:38 -0700

    UPDATE file_directory

--- ps_file_directory.py
+++ ps_file_directory.py
@@ -1,44 +1,47 @@
 """
 Program to search a file directory and return file paths
 @author: Peter Senyk
-@version: 2024_0.2
+@version: 2024_0.3
 """
 
 import csv
 
 
 def find_file_in_tree(file_path, filename):
-    with open(file_path, 'r', encoding='utf-16') as file:
-        lines = file.readlines()
-    file_path_dictionary = check_lines(lines, filename)
-    return file_path_dictionary
+    try:
+        with open(file_path, 'r', encoding='utf-16') as file:
+            lines = file.readlines()
+    except FileNotFoundError:
+        print(f"Error: File {file_path} was not found.")
+        return {}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {}
+
+    return process_lines(lines, filename)
 
 
-def check_lines(lines, filename):
+def process_lines(lines, filename):
     file_path_dictionary = {}
     current_dir = ''
     count = 0
 
     for i, line in enumerate(lines):
         if "Directory:" in line:
-            if current_dir and count > 0:
-                file_path_dictionary[current_dir] = count
+            # if current_dir and count > 0:
+            #     file_path_dictionary[current_dir] = count
             current_dir = clean_directory_line(line.strip())
             current_dir = check_multiline_directory(lines, i + 1, current_dir)
             count = 0
         elif line.strip() == '' and count > 0:
             if current_dir:
-                current_dir = current_dir.removeprefix("Directory: ").removeprefix("Directory:").removeprefix(" ")
+                # current_dir = clean_directory_line(current_dir)
                 file_path_dictionary[current_dir] = count
                 current_dir = ''
                 count = 0
         elif filename.lower() in line.lower():
             count += 1
 
-    if current_dir and count > 0:
-        file_path_dictionary[current_dir] = count
-
-    print(file_path_dictionary)
     return file_path_dictionary
 
 
@@ -50,8 +53,7 @@ def check_multiline_directory(lines, index, current_dir):
 
 
 def clean_directory_line(line):
-    dir_line = line.removeprefix("    Directory: ")
-    return dir_line
+    return line.removeprefix(" ").removeprefix("Directory:").removeprefix(" ")
 
 
 def clean_dictionary_results(file_path_dictionary):
@@ -61,22 +63,23 @@ def clean_dictionary_results(file_path_dictionary):
 
 def export_file_paths(path_dictionary, search, output_path):
     file_name = f"Filepath_Output_For_Search_{search}.csv"
-    file_output = output_path + "\\" + file_name
+    file_output = f"{output_path}\\{file_name}"
+    try:
+        with open(file_output, "w", newline="") as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(["File Path", "Number of Files"])
 
-    with open(file_output, "w", newline="") as output_file:
-        writer = csv.writer(output_file)
-        writer.writerow(["File Path", "Number of Files"])
-
-        for path, number in path_dictionary.items():
-            writer.writerow([path, number])
+            for path, number in path_dictionary.items():
+                writer.writerow([path, number])
+    except Exception as e:
+        print(f"Error writing to file: {e}")
 
 
 if __name__ == '__main__':
-    tree_file_path = r"C:\File_Directory_Search\directory_listing.txt"  # Path to the tree file
-    output_file_path = r"C:\File_Directory_Search\Search_Results"  # Output file path
+    tree_file_path = r"D:\testScripts\batch\directory_listing.txt"  # Path to the tree file
+    output_file_path = r"D:\testScripts\testOutput"  # Output file path
     search_filename = input("Enter your search here: ")  # Filename to search for
 
     file_paths = find_file_in_tree(tree_file_path, search_filename)
     export_file_paths(file_paths, search_filename, output_file_path)
-    stay_open = input()
-
+    stay_open = input("Press Enter to exit...")
